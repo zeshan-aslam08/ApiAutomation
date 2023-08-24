048f7ecfbe122f1ae03b7cddc3b2125b2dd7f753
zeshan-aslam08 <zeshanaslam745@gmail.com>
2023-08-24 13:26:19 +0500

    Done Commit

--- TestCases/test_Agent_Login.py
+++ TestCases/test_Agent_Login.py
@@ -5,5 +5,22 @@ from Methods.LoginAgent import LoginPage
 
 class TestLogin(LoginPage):
     def test_login_with_valid_credentials(self):
+        """
+         Login With Valid credentials Test Case
+        """
         flag = self.login_with_valid_credentials()
+        assert flag is True
+
+    def test_login_with_invalid_credentials(self):
+        """
+         Login With in-Valid credentials Test Case
+        """
+        flag = self.login_with_invalid_credentials()
+        assert flag is True
+
+    def test_login_with_invalid_Name(self):
+        """
+         Login With in-Valid credentials Test Case
+        """
+        flag = self.login_with_invalid_Name()
         assert flag is True
